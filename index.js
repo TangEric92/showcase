652fbc28a5a78a6fe4cfbfc44aadaa53aeb8c38b
TangEric92 <55618247+TangEric92@users.noreply.github.com>
2019-11-18 18:58:47 +0100

    change theme on click

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('it works')
+console.log("it works");
+
+const tag = document.querySelector(".change-theme");
+const theme = document.querySelector(".promotion");
+const header = document.querySelector("header");
+// add class change on h1 click
+tag.addEventListener("click", () => {
+  theme.classList.toggle("dark");
+  header.classList.toggle("dark");
+});
