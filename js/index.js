20da48f368883e0e2391af4624fd3be7eaf9e8f3
TangEric92 <55618247+TangEric92@users.noreply.github.com>
2019-11-24 11:19:05 +0100

    dark theme

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 console.log("it works");
 
-const tag = document.querySelector(".change-theme");
-const theme = document.querySelector(".promotion");
+const changeTheme = document.querySelector(".change-theme");
+
 const header = document.querySelector("header");
+const theme = document.querySelector(".shipping-bar");
+const box = document.querySelector(".box");
+const footer = document.querySelector(".footer");
+const label = document.querySelector(".label");
+
 // add class change on h1 click
-tag.addEventListener("click", () => {
+changeTheme.addEventListener("click", () => {
   theme.classList.toggle("dark");
   header.classList.toggle("dark");
+  label.classList.toggle("dark");
+
+  box.classList.toggle("dark");
+  footer.classList.toggle("dark");
 });
